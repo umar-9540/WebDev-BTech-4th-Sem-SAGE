f629ea7f5cfc7d3c559f91621fc2044c2504e70a
Mohd Umar Siddiqui <u9540776786@gmail.com>
2026-06-12 15:44:34 +0530

    JavaScript basics

---
+++ 3_JavaScript/1_Variables.js
@@ -0,0 +1,34 @@
+// var a = 10;
+
+// console.log(a);
+
+// a = true;
+// console.log(a);
+
+// let a = "String";
+// console.log(a);
+
+// a = 10;
+// console.log(a);
+
+// const a = 10;
+// console.log(a);
+
+// a = "string";
+// console.log(a);
+
+// let a;
+// console.log(a);
+
+// let b = null;
+// console.log(b);
+
+// let a = "5" + 2;
+// console.log(a);
+
+let a = "10";
+if (a === 10) {
+  console.log("TRUE");
+} else {
+  console.log("FALSE");
+}
---
+++ 3_JavaScript/2_Loops.js
@@ -0,0 +1,12 @@
+for (let a = 0; a < 5; a++) {
+  console.log(a);
+}
+
+let count = 3;
+while (count > 0) {
+  let b = 10;
+  console.log("countdown - ", count);
+  count--;
+}
+
+console.log(b);
