b99954a6313d0e166f5045d5cddb6ccc0007f37f
Mohammad Umar <97541126+umar-9540@users.noreply.github.com>
2026-06-14 02:05:07 +0530

    Revise comments on variable declarations in JavaScript
    Updated comments to clarify variable declarations and scopes in JavaScript.

--- 3_JavaScript/1_Variables.js
+++ 3_JavaScript/1_Variables.js
@@ -1,16 +1,20 @@
-// var a = 10;
+// var, let, and const are JavaScript keywords used to declare variables
 
+// var is Function-scoped and allows re-declaration
+// var a = 10;
 // console.log(a);
 
 // a = true;
 // console.log(a);
 
+// let is Block-scoped and can be reassigned, but cannot be re-declared
 // let a = "String";
 // console.log(a);
 
 // a = 10;
 // console.log(a);
 
+// const is Block-scoped, cannot be reassigned, and must be initialized upon declaration
 // const a = 10;
 // console.log(a);
 
@@ -18,10 +22,10 @@
 // console.log(a);
 
 // let a;
-// console.log(a);
+// console.log(a);  // undefined - Value hasn't been set yet, Automatically assigned
 
 // let b = null;
-// console.log(b);
+// console.log(b);  // Intentional "empty" value, Manually assigned
 
 // let a = "5" + 2;
 // console.log(a);
