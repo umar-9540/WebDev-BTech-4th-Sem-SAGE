0b5a600051a7115d6c8ad75d392082c7d856bf04
Mohd Umar Siddiqui <u9540776786@gmail.com>
2026-06-18 15:44:27 +0530

    ES6 and Object Manipulation

--- 3_JavaScript/10_Hoisting.js
+++ 3_JavaScript/10_Hoisting.js
@@ -28,8 +28,32 @@
 // dance();
 // console.log(dance);
 
-fly(); // Reference Error
-const fly = () => {
-  console.log("Flying");
-};
-fly();
+// fly(); // Reference Error
+// const fly = () => {
+//   console.log("Flying");
+// };
+// fly();
+
+function check() {
+  console.log(color);
+  //   let color = "Red";
+}
+var color = "Blue";
+check();
+
+// DESTRUCTURING
+let obj = { id: 1, name: "Lucky", color: "Blue", subject: "DSA" };
+let { id, name, ...obj2 } = obj;
+console.log(obj2);
+
+let arr1 = ["Apple", 2];
+let arr2 = [...arr1, 3, 4];
+console.log(arr2);
+
+let car = { make: "Toyota", model: "Corolla" };
+
+console.log(Object.keys(car)); // ["make", "model"]
+
+console.log(Object.values(car)); // ["Toyota", "Corolla"]
+
+console.log(Object.entries(car)); // [["make", "Toyota"], ["model", "Corolla"]]
