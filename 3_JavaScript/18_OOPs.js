a57903518c4bb5b152058bb4399486ebdba0797d
Mohd Umar Siddiqui <u9540776786@gmail.com>
2026-06-30 12:40:20 +0530

    OOPs in Js

---
+++ 3_JavaScript/18_OOPs.js
@@ -0,0 +1,45 @@
+// const student1 = {
+//   name: "Piyush",
+//   attendance: 0,
+
+//   isPresent: function () {
+//     this.attendance++;
+//     console.log(
+//       `${this.name} is present. Total Attendance = ${this.attendance}`,
+//     );
+//   },
+// };
+
+// student1.isPresent();
+// student1.isPresent();
+
+// const student2 = {
+//   name: "Piyush",
+//   attendance: 0,
+
+//   isPresent: function () {
+//     this.attendance++;
+//     console.log(
+//       `${this.name} is present. Total Attendance = ${this.attendance}`,
+//     );
+//   },
+// };
+
+class Student {
+  //   constructor(name, attendance) {
+  //     this.name = name; // this is the pointer pointing to the Object
+  //     this.attendance = attendance;
+  //   }
+
+  isPresent() {
+    // isPresent is stored only once in the memory
+    // this.attendance++;
+    console.log(`is present.`);
+  }
+}
+
+const student1 = new Student();
+const student2 = new Student();
+
+student1.isPresent();
+student2.isPresent();
