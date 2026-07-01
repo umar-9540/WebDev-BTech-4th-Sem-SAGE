b633358a945fe1a26d886ac035808b4b3572b561
Mohd Umar Siddiqui <u9540776786@gmail.com>
2026-07-01 16:04:33 +0530

    DOM Manipulation

--- 3_JavaScript/19_DOM.js/3_ClassList.js
+++ 3_JavaScript/19_DOM.js/3_ClassList.js
@@ -1,13 +1,13 @@
 const item = document.querySelector("body");
 console.log(item);
 
-// setTimeout(() => {
-//   item.classList.add("movie");
-// }, 1000);
+setTimeout(() => {
+  item.classList.add("movie");
+}, 1000);
 
-// setTimeout(() => {
-//   item.classList.remove("movie");
-// }, 2000);
+setTimeout(() => {
+  item.classList.remove("movie");
+}, 2000);
 
 setInterval(() => {
   item.classList.toggle("movie");
