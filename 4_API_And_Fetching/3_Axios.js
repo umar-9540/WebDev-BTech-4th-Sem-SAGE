be02d0c0ba35553e1362827e9f34d1648f3ca85a
Mohd Umar Siddiqui <u9540776786@gmail.com>
2026-07-07 14:56:44 +0530

    API and Fetching

---
+++ 4_API_And_Fetching/3_Axios.js
@@ -0,0 +1,25 @@
+let url = "https://jsonplaceholder.typicode.com/todos";
+
+const ul = document.querySelector("#list");
+const btn = document.querySelector("#btn");
+
+function addTaskToList(data) {
+  for (let i = 0; i < data.length; i++) {
+    const li = document.createElement("li");
+    li.innerText = data[i].title;
+    ul.appendChild(li);
+  }
+}
+
+btn.addEventListener("click", () => {
+  axios
+    .get(url)
+    .then((res) => {
+      // console.log(data.data);
+      data = res.data;
+      addTaskToList(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
